6239fa3b265f4f7b145fc3597c4cba65dba9a1fd
scalser <scalserstv@yandex.ru>
2022-08-09 20:11:39 +0300

    edited - chromedriver binary

--- conftest.py
+++ conftest.py
@@ -1,27 +1,14 @@
 """fixtures for choosing which browser to work with"""
 import pytest
 from selenium import webdriver
-
-
-def pytest_addoption(parser):
-    """choosing browser"""
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    """fixture for choosing a browser for tests, default browser -> GoogleChrome"""
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        ffp = webdriver.FirefoxProfile()
-        browser = webdriver.Firefox(firefox_profile=ffp)
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+def browser():
+    """default browser -> GoogleChrome"""
+    s = Service('C:/chromedriver/chromedriver.exe')
+    browser = webdriver.Chrome(service=s)
+
     yield browser
     browser.quit()
